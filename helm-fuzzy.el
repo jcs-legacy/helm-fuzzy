0212478f8c62c35c763634fc78e76604f6d5a876
Jen-Chieh <jcs090218@gmail.com>
2019-08-26 17:35:58 +0800

    Complete core.

--- helm-fuzzy.el
+++ helm-fuzzy.el
@@ -6,8 +6,8 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Fuzzy matching for helm source.
 ;; Keyword: fuzzy helm matching source
-;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.3") (helm "1.7.9") (flx "0.5"))
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.4") (helm "1.7.9") (flx "0.5"))
 ;; URL: https://github.com/jcs090218/helm-fuzzy
 
 ;; This file is NOT part of GNU Emacs.
@@ -32,9 +32,6 @@
 
 ;;; Code:
 
-(require 'flx)
-(require 'helm)
-
 
 (defgroup helm-fuzzy nil
   "Fuzzy matching for helm source."
@@ -43,29 +40,72 @@
   :link '(url-link :tag "Repository" "https://github.com/jcs090218/helm-fuzzy"))
 
 
+(defcustom helm-fuzzy-not-allow-fuzzy '("*helm-ag*")
+  "List of buffer action that doesn't allow fuzzy."
+  :type 'list
+  :group 'helm-fuzzy)
 
-(defun helm-fuzzy--helm-after-initialize-hook ()
-  ""
-  (with-current-buffer (get-buffer helm-buffer)
-    (message "-- %s" (buffer-string)))
-  )
 
+(defun helm-fuzzy--is-contain-list-string (in-list in-str)
+  "Check if a string IN-STR contain in any string in the string list IN-LIST."
+  (cl-some #'(lambda (lb-sub-str) (string-match-p (regexp-quote lb-sub-str) in-str)) in-list))
+
+(defun helm-fuzzy--sort-candidates (candidates)
+  "Fuzzy matching for all CANDIDATES."
+  (when (and (not (string= helm-pattern ""))
+             (not (helm-fuzzy--is-contain-list-string helm-fuzzy-not-allow-fuzzy helm-buffer)))
+    (require 'flx)
+    (let* ((scoring-table (make-hash-table))
+           (scoring-keys '())
+           (first-sel (nth 0 candidates))
+           (pattern (if (listp first-sel) (cdr first-sel) first-sel)))
+      (dolist (cand candidates)
+        (let* ((cand-id (if (listp cand) (cdr cand) cand))
+               (scoring (flx-score cand-id pattern))
+               ;; Ensure score is not `nil'.
+               (score (if scoring (nth 0 scoring) 0)))
+          ;; For first time access score with hash-table.
+          (unless (gethash score scoring-table) (setf (gethash score scoring-table) '()))
+          ;; Push the candidate with the target score to hash-table.
+          (push cand (gethash score scoring-table))))
+      ;; Get all the keys into a list.
+      (maphash (lambda (score-key _cand-lst) (push score-key scoring-keys)) scoring-table)
+      (setq scoring-keys (sort scoring-keys #'>))  ; Sort keys in order.
+      (setq candidates '())  ; Clean up, and ready for final output.
+      (dolist (key scoring-keys)
+        (let ((cands (sort (gethash key scoring-table)
+                           (lambda (lst1 lst2)
+                             (let ((str1 (if (listp lst1) (cdr lst1) lst1))
+                                   (str2 (if (listp lst2) (cdr lst2) lst2)))
+                               (string-lessp str1 str2))))))
+          (setq candidates (append candidates cands))))))
+  candidates)
+
+(defun helm-fuzzy--helm-process-filtered-candidate-transformer (candidates source)
+  "Filtered by CANDIDATES in SOURCE."
+  (helm-aif (assoc-default 'filtered-candidate-transformer source)
+      (let ((cands (helm-apply-functions-from-source source it candidates source)))
+        (setq cands (helm-fuzzy--sort-candidates cands))
+        cands)
+    (setq candidates (helm-fuzzy--sort-candidates candidates))
+    candidates))
 
 (defun helm-fuzzy--enable ()
   "Enable `helm-fuzzy'."
-  (add-hook 'helm-after-initialize-hook #'helm-fuzzy--helm-after-initialize-hook)
-  )
+  (require 'helm)
+  (advice-add 'helm-process-filtered-candidate-transformer :override 'helm-fuzzy--helm-process-filtered-candidate-transformer))
 
 (defun helm-fuzzy--disable ()
   "Disable `helm-fuzzy'."
-  (remove-hook 'helm-after-initialize-hook #'helm-fuzzy--helm-after-initialize-hook)
-  )
+  (advice-remove 'helm-process-filtered-candidate-transformer 'helm-fuzzy--helm-process-filtered-candidate-transformer))
 
 ;;;###autoload
 (define-minor-mode helm-fuzzy-mode
   "Minor mode 'helm-fuzzy-mode'."
   :lighter " HelmFuzzy"
-  :group helm-fuzzy-mode
+  :global t
+  :require 'helm-fuzzy
+  :group 'helm-fuzzy
   (if helm-fuzzy-mode
       (helm-fuzzy--enable)
     (helm-fuzzy--disable)))
